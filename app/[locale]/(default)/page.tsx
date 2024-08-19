ebec2cd2c006e6558380b4fac797160c5bb06878
HeartexceL <roelandticlao@gmail.com>
2024-08-19 11:02:54 +0800

    Catalyst app Upload

---
+++ app/[locale]/(default)/page.tsx
@@ -0,0 +1,107 @@
+import { removeEdgesAndNodes } from '@bigcommerce/catalyst-client';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages, getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+
+import { getSessionCustomerId } from '~/auth';
+import { client } from '~/client';
+import { graphql } from '~/client/graphql';
+import { revalidate } from '~/client/revalidate-target';
+import { Hero } from '~/components/hero';
+import {
+  ProductCardCarousel,
+  ProductCardCarouselFragment,
+} from '~/components/product-card-carousel';
+import { ProductCard } from '~/components/product-card';
+
+import { LocaleType } from '~/i18n';
+
+interface Props {
+  params: {
+    locale: LocaleType;
+  };
+}
+
+const HomePageQuery = graphql(
+  `
+    query HomePageQuery {
+      site {
+        newestProducts(first: 12) {
+          edges {
+            node {
+              ...ProductCardCarouselFragment
+            }
+          }
+        }
+        featuredProducts(first: 12) {
+          edges {
+            node {
+              ...ProductCardCarouselFragment
+            }
+          }
+        }
+      }
+    }
+  `,
+  [ProductCardCarouselFragment],
+);
+
+export default async function Home({ params: { locale } }: Props) {
+  unstable_setRequestLocale(locale);
+
+  const customerId = await getSessionCustomerId();
+
+  const t = await getTranslations({ locale, namespace: 'Home' });
+  const messages = await getMessages({ locale });
+
+  const { data } = await client.fetch({
+    document: HomePageQuery,
+    customerId,
+    fetchOptions: customerId ? { cache: 'no-store' } : { next: { revalidate } },
+  });
+
+  const featuredProducts = removeEdgesAndNodes(data.site.featuredProducts);
+  const newestProducts = removeEdgesAndNodes(data.site.newestProducts);
+
+  return (
+    <>
+      <Hero />
+
+      <div class="text-div"></div>
+      <div className="my-10">
+        <NextIntlClientProvider locale={locale} messages={{ Product: messages.Product ?? {} }}>
+          <ProductCardCarousel
+            products={featuredProducts}
+            showCart={false}
+            showCompare={false}
+            title={t('Carousel.featuredProducts')}
+          />
+          <ProductCardCarousel
+            products={newestProducts}
+            showCart={false}
+            showCompare={false}
+            title={t('Carousel.newestProducts')}
+          />
+
+          <div className="homepage-section">
+            <h2 className="text-3xl font-black mb-10 lg:text-4xl" id="newestproducts-hp-1">
+              New Products
+            </h2>
+            <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 sm:gap-8">
+              {newestProducts.map((product, index) => (
+                <ProductCard
+                  imagePriority={index <= 3}
+                  imageSize="wide"
+                  key={product.entityId}
+                  product={product}
+                />
+              ))}
+            </div>
+          </div> 
+
+        </NextIntlClientProvider>
+      </div>
+    </>
+  );
+}
+
+export const runtime = 'edge';
